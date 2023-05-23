baedd06d65d545ade65d198cd567c1146b5d4d90
Manuel Garcia <manueldev250799@gmail.com>
2023-05-23 00:49:23 -0500

    build: initial commit

---
+++ app/src/main/java/com/example/grupo_01_garciachuman_tarea_04_ejercicio_02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.grupo_01_garciachuman_tarea_04_ejercicio_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
